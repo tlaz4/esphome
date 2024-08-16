5e467ae46c73b78cf1a1c38cd3c943d8f158834b
Tyler <tlazaren@ualberta.ca>
2024-08-15 21:10:02 -0600

    add rcwl1601

---
+++ esphome/components/rcwl1601/rcwl1601.cpp
@@ -0,0 +1,22 @@
+#include "esphome/core/log.h"
+#include "rcwl1601.h"
+
+namespace esphome {
+namespace rcwl1601_sensor {
+
+static const char *TAG = "rcwl1601.sensor";
+
+void RCWL1601::setup(){
+
+}
+
+void RCWL1601::update(){
+  publish_state(42.0)
+}
+
+void RCWL1601::dump_config(){
+
+}
+
+}
+}
---
+++ esphome/components/rcwl1601/sensor.py
@@ -0,0 +1,30 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import i2c, sensor
+from esphome.const import ICON_RULER, UNIT_MILLIMETER
+
+DEPENDENCIES = ["i2c"]
+
+
+rcwl1601_sensor_ns = cg.esphome_ns.namespace("rcwl1601")
+RCWL1601Sensor = rcwl1601_sensor_ns.class_(
+    "RCWL-1601", cg.PollingComponent, i2c.I2CDevice
+)
+
+CONFIG_SCHEMA = (
+    sensor.sensor_schema(
+        RCWL1601Sensor,
+        unit_of_measurement=UNIT_MILLIMETER,
+        icon=ICON_RULER,
+        accuracy_decimals=1
+    )
+    .extend(cv.polling_component_schema("5s"))
+    .extend(i2c.i2c_device_schema(0x57))
+)
+
+
+async def to_code(config):
+    var = await sensor.new_sensor(config)
+    await cg.register_component(var, config)
+    await i2c.register_i2c_device(var, config)
+
