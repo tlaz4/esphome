03c78fff7fcdd9bf4c5d7f9093385d2da26568b7
Tyler <tlazaren@ualberta.ca>
2024-08-15 23:29:53 -0600

    attempt to read from i2c

--- esphome/components/rcwl1601/sensor.py
+++ esphome/components/rcwl1601/sensor.py
@@ -17,7 +17,7 @@ CONFIG_SCHEMA = (
         unit_of_measurement=UNIT_MILLIMETER,
         icon=ICON_RULER,
         accuracy_decimals=1,
-        device_class=DEVICE_CLASS_TEMPERATURE,
+        device_class=DEVICE_CLASS_DISTANCE,
         state_class=STATE_CLASS_MEASUREMENT
     )
     .extend(cv.polling_component_schema("5s"))
