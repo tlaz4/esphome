692869cc0126117d6c96930879a8f4772f4e832c
Tyler <tlazaren@ualberta.ca>
2024-08-16 15:18:42 -0600

    more

--- esphome/components/rcwl1601/rcwl1601.cpp
+++ esphome/components/rcwl1601/rcwl1601.cpp
@@ -27,9 +27,9 @@ void RCWL1601::dump_config(){
 }
 
 bool RCWL1601::read_data_(uint8_t *data){
-  this->write_byte(0x57, 1);
+  this->write((byte)1, 1);
   delay_microseconds_safe(20000);
-  this->read_bytes(0x57, data, 3);
+  this->read(data, sizeof(data));
 
   return true;
 
